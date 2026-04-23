3528cae2c439ea1e4595d5aeb40fbaecfa811adf
Sebastian Thunberg <sebastian.thunberg@appliedtechnology.se>
2026-04-23 14:39:35 +0200

    add cors config

---
+++ src/main/java/org/hackweek/backend/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package org.hackweek.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // apply to all endpoints
+                .allowedOrigins(
+                        "https://sebbsoon.github.io",
+                        "http://localhost:5173"
+                ).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
