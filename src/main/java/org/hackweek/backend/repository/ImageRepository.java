7deae78786a716a7992abbd5659bb69cc00f6fae
Sebastian Thunberg <sebastian.thunberg@appliedtechnology.se>
2026-04-27 20:14:22 +0200

    custom find by gallery id

--- src/main/java/org/hackweek/backend/repository/ImageRepository.java
+++ src/main/java/org/hackweek/backend/repository/ImageRepository.java
@@ -1,7 +1,9 @@
 package org.hackweek.backend.repository;
 
+import java.util.List;
 import org.hackweek.backend.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends ListCrudRepository<Image, Long> {
+    List<Image> findByGalleryId(Long galleryId);
 }
